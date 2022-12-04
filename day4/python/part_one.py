e4eb938e3414415985c8f06122cf767d6b8aa3ec
James <35628223+ekralc@users.noreply.github.com>
2022-12-04 05:07:38 +0000

    Initial solution for day 4

---
+++ day4/python/part_one.py
@@ -0,0 +1,20 @@
+import sys
+
+count = 0
+for line in sys.stdin:
+    line = line.rstrip()
+
+    parts = line.split(",")
+
+    a = [int(x) for x in parts[0].split("-")]
+    b = [int(x) for x in parts[1].split("-")]
+
+    first = set([i for i in range(a[0], a[1] + 1)])
+    second = set([i for i in range(b[0], b[1] + 1)])
+
+    intersection = first.intersection(second)
+
+    if intersection == first or intersection == second:
+        count += 1
+
+print(count)
