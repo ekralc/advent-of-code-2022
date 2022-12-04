d349eb4775f93eaef4f98687c1cb0d993a173996
James <35628223+ekralc@users.noreply.github.com>
2022-12-04 05:20:20 +0000

    Refactor day 4 solutions

--- day4/python/part_one.py
+++ day4/python/part_one.py
@@ -4,17 +4,12 @@ count = 0
 for line in sys.stdin:
     line = line.rstrip()
 
-    parts = line.split(",")
+    ranges = [[int(x) for x in i.split("-")] for i in line.split(",")]
+    a, b = [set([i for i in range(x[0], x[1] + 1)]) for x in ranges]
 
-    a = [int(x) for x in parts[0].split("-")]
-    b = [int(x) for x in parts[1].split("-")]
+    intersection = a.intersection(b)
 
-    first = set([i for i in range(a[0], a[1] + 1)])
-    second = set([i for i in range(b[0], b[1] + 1)])
-
-    intersection = first.intersection(second)
-
-    if intersection == first or intersection == second:
+    if intersection == a or intersection == b:
         count += 1
 
 print(count)
