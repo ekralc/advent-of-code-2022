e74ca5f238888373466388ba0dcddf129ceb1d1e
James <3824237+jamesandrewclarke@users.noreply.github.com>
2022-12-05 12:29:02 +0000

    Slightly nicer list comprehension for day 4

--- day4/python/part_one.py
+++ day4/python/part_one.py
@@ -5,7 +5,7 @@ for line in sys.stdin:
     line = line.rstrip()
 
     ranges = [[int(x) for x in i.split("-")] for i in line.split(",")]
-    a, b = [set([i for i in range(start, finish + 1)]) for (start, finish) in ranges]
+    a, b = [{list(range(start, finish + 1))} for (start, finish) in ranges]
 
     if a.issubset(b) or a.issuperset(b):
         count += 1
