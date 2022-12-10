293eded58bee03bc66ec36ed52c129daad30c5ee
James <35628223+ekralc@users.noreply.github.com>
2022-12-10 13:44:59 +0000

    Refactor day 10

--- day10/python/part_one.py
+++ day10/python/part_one.py
@@ -1,31 +1,23 @@
 import sys
 
-stack = [0, 0]
-
 register = 1
-
 count = 1
-checkpoints = [20, 60, 100, 140, 180, 220]
-
 total_signal_strength = 0
 
+checkpoints = [20, 60, 100, 140, 180, 220]
+
+
 for line in sys.stdin:
-    line = line.rstrip()
-
-    print(count, register)
-
     if count in checkpoints:
-        print(count, register)
         total_signal_strength += count * register
         checkpoints.pop(0)
 
-    if not line.startswith("noop"):
-        instruction, arg = line.split()
-        arg = int(arg)
-
+    if line.startswith("addx"):
+        arg = int(line.rstrip().split()[1])
         count += 1
         if count in checkpoints:
             total_signal_strength += count * register
+            checkpoints.pop(0)
 
         register += arg
 
