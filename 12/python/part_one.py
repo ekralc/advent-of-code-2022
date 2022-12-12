ca8fd490aa0b3c312ca8439f8570ed79087ce0c6
James <3824237+jamesandrewclarke@users.noreply.github.com>
2022-12-12 08:50:47 +0000

    Add day 12 solutions

---
+++ 12/python/part_one.py
@@ -0,0 +1,89 @@
+from collections import defaultdict
+import sys
+
+grid = []
+start_position = ()
+goal_position = ()
+
+for line in sys.stdin:
+    line = line.rstrip()
+
+    row = list(line)
+    grid.append(row)
+    if "S" in row:
+        x = row.index("S")
+        start_position = (x, len(grid) - 1)
+    if "E" in row:
+        x = row.index("E")
+        goal_position = (x, len(grid) - 1)
+
+m = len(grid)
+n = len(grid[0])
+
+
+def climbable(a, b):
+    val_a = grid[a[1]][a[0]]
+    val_b = grid[b[1]][b[0]]
+
+    if val_a == "S":
+        val_a = "a"
+    elif val_a == "E":
+        val_a = "z"
+
+    if val_b == "S":
+        val_b = "a"
+    elif val_b == "E":
+        val_b = "z"
+
+    difference = ord(val_b) - ord(val_a)
+    return difference <= 1
+
+
+def adjacent(pos):
+    """
+    Returns an iterator of adjacent candidate squares
+    """
+    x, y = pos
+    candidates = []
+    if x > 0:
+        candidates.append((x - 1, y))
+    if x < n - 1:
+        candidates.append((x + 1, y))
+    if y > 0:
+        candidates.append((x, y - 1))
+    if y < m - 1:
+        candidates.append((x, y + 1))
+
+    return list(filter(lambda x: climbable(pos, x), candidates))
+
+
+explored = defaultdict(lambda: False)
+
+parent = dict()
+
+
+def bfs(root):
+    queue = []
+    explored[root] = True
+
+    queue.append(root)
+    while len(queue) > 0:
+        v = queue.pop(0)
+        if v == goal_position:
+            return v
+
+        for w in adjacent(v):
+            if not explored[w]:
+                parent[w] = v
+                explored[w] = True
+                queue.append(w)
+
+
+end = bfs(start_position)
+next = parent[end]
+length = 1
+while next != start_position:
+    next = parent[next]
+    length += 1
+
+print(length)
