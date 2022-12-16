ce0247ccd78ac0ad4d79dc7a578b30b0f0316f29
James <35628223+ekralc@users.noreply.github.com>
2022-12-16 18:30:48 +0000

    Create much faster solution for a single player search

--- 16/python/part_two.py
+++ 16/python/part_two.py
@@ -27,95 +27,45 @@ for k in adj.keys():
             if dist[i][j] > dist[i][k] + dist[k][j]:
                 dist[i][j] = dist[i][k] + dist[k][j]
 
-
-
-def get_actions(state):
-    you, elephant, _, time_left, _ = state
-    actions = []
-    if time_left < 1:
-        return actions
-
-    if flow[you] > 0:
-        # Only open valve if it would increase the flow
-        actions.append(("open", 1, you))
-
-    if flow[elephant] > 0:
-        actions.append(("open", 2, elephant))
-
-    for neighbour in adj[you]:
-        actions.append(("move", 1, neighbour))
-
-    for neighbour in adj[elephant]:
-        actions.append(("move", 2, neighbour))
-
-
-    return actions
-
-def action(state, action):
-    """
-    Returns the next state given an action
-    """
-
-    you, elephant, open, time_left, total_flow = state
-    command, player, cave = action
-
-    open = set(open)
-    match command:
-        case 'open':
-            assert cave in [you, elephant] 
-            open.add(cave)
-            time_left -= 1
-        case 'move':
-            time_left -= 1
-
-            match player:
-                case 1:
-                    you = cave
-                    assert cave in adj[you]
-                case 2:
-                    elephant = cave
-                    assert cave in adj[elephant]
-
-    open = frozenset(open)
-
-    total_flow += calculate_flow(open)
-
-    return you, elephant, open, time_left, total_flow
-
-visited = set()
-highest_flow = 0
-
-@functools.cache
-def search(state):
-    actions = get_actions(state)
-
-    print(actions)
-    highest_flow = 0
-    if len(actions) == 0:
-        _, _, open, _, total_flow = state
-        return total_flow - calculate_flow(open)
-    for a in actions:
-        next_state = action(state, a)
-        if identify_state(next_state) not in visited:
-            visited.add(identify_state(next_state))
-            highest_flow = max(search(next_state), highest_flow)
-    
-    return highest_flow
-
-@functools.cache
-def calculate_flow(open):
-    return sum([flow[cave] for cave in open])
-
-def identify_state(state):
-    return state
-
 def show_dist():
     for key, value in dist.items():
         print(key, value)
         print()
 
+# show_dist()
 
 
+working = { key: value for key, value in adj.items() if flow[key] > 0}
 
-initial_state = "AA", "AA", frozenset(), 26, 0
-print(search(initial_state))
+@functools.cache
+def dfs(start, time_limit, visited):
+    visited = set(visited)
+
+    highest = 0
+    for cave in working:
+        time_left = time_limit
+        if cave in visited: continue
+
+        new_visited = visited.copy()
+        new_visited.add(cave)
+
+        time = dist[start][cave]
+        if time_left - (time + 1) < 0:
+            continue
+
+        # We open the valve at the destination, so add 1
+        time_left -= time + 1 
+        result = time_left * flow[cave]
+
+        rest = dfs(cave, time_left, frozenset(new_visited))
+        result += rest
+
+        highest = max(result, highest)
+
+    return highest
+
+print(dfs("AA", 30, frozenset()))
+
+sys.exit(0)
+adj = { key:[a for a in value if a not in open] for key, value in adj.items() if key not in open}
+flow = { key:value for key, value in flow.items() if key not in open}
