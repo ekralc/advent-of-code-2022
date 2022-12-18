ef2f62497659d86a1456decc8c52f03afd3c4543
James <3824237+jamesandrewclarke@users.noreply.github.com>
2022-12-18 13:59:29 +0000

    Refactor part 1

--- 18/python/part_one.py
+++ 18/python/part_one.py
@@ -1,22 +1,17 @@
 import sys
 import re
 
-cubes = set()
-for line in sys.stdin:
-    cube = tuple([int(a) for a in re.findall("\d+", line.rstrip())])
-    cubes.add(cube)
+cubes = { tuple(map(int, re.findall("\d+", line))) for line in sys.stdin }
 
 surface_area = 0
-for cube in cubes:
-    connected_sides = 0
+for v in cubes:
+    open_sides = 6
+    x, y, z = v
 
-    x, y, z = cube
-    for i in [(x+1,y,z),(x-1,y,z),(x,y+1,z),(x,y-1,z),(x,y,z+1),(x,y,z-1)]:
-       if i in cubes:
-        connected_sides += 1 
+    for w in [(x+1,y,z),(x-1,y,z),(x,y+1,z),(x,y-1,z),(x,y,z+1),(x,y,z-1)]:
+       if w in cubes:
+        open_sides -= 1 
 
-    open_sides = 6 - connected_sides
     surface_area += open_sides
 
-print()
 print(surface_area)
