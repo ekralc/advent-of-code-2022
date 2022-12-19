30ad894bc528106cbcb03b9c76cd598628f8a411
James <3824237+jamesandrewclarke@users.noreply.github.com>
2022-12-19 06:34:53 +0000

    slow BFS

---
+++ 19/python/part_one.py
@@ -0,0 +1,88 @@
+import sys
+import re
+import copy
+import functools
+
+blueprints = []
+
+for line in sys.stdin:
+    blueprint = tuple(map(int, re.findall(r"\d+", line.rstrip())))
+    blueprints.append(blueprint)
+    
+# BFS
+class State:
+    ore = 0
+    clay = 0
+    obsidian = 0
+    geodes = 0
+
+    ore_robots = 1
+    clay_robots = 0
+    obsidian_robots = 0
+    geode_robots = 0
+
+    time_left = 16
+
+@functools.cache
+def get_actions(bp, current_state):
+    _, ore_ore, clay_ore, obs_ore, obs_clay, geo_ore, geo_obs = bp
+
+    if current_state.time_left <= 0:
+        return {}
+
+    actions = {"none"}
+    if current_state.ore >= ore_ore:
+        actions.add("build ore")
+
+    if current_state.ore >= clay_ore:
+        actions.add("build clay")
+
+    if current_state.ore >= obs_ore and current_state.clay >= obs_clay:
+        actions.add("build obs")
+
+    if current_state.ore >= geo_ore and current_state.obsidian >= geo_obs:
+        actions.add("build geo")
+
+    return frozenset(actions)
+
+def update_state(current_state, action):
+    state = copy.deepcopy(current_state)
+
+    # Mine 1 resourch for each of its robot types
+    state.ore += state.ore_robots
+    state.clay += state.clay_robots
+    state.obsidian += state.obsidian_robots
+    state.geodes += state.geode_robots
+
+    state.time_left -= 1
+
+    match action:
+        case "build ore":
+            state.ore_robots += 1
+        case "build clay":
+            state.clay_robots += 1
+        case "build obs":
+            state.obsidian_robots += 1
+        case "build geo":
+            state.geode_robots += 1
+
+    return state
+
+@functools.cache
+def optimal_geodes(bp, state):
+    max_geodes = 0
+
+    actions = get_actions(bp, state)
+    if len(actions) == 0:
+        return state.geodes
+
+    for action in actions:
+        print(action)
+        next_state = update_state(state, action)
+        assert next_state.time_left >= 0
+        max_geodes = max(max_geodes, optimal_geodes(bp, next_state))
+
+    return max_geodes
+
+initial_state = State()
+print(optimal_geodes(blueprints[26], initial_state))
