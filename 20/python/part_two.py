6bc56212a5f4fbc104848657f08f5f2753504abe
James <3824237+jamesandrewclarke@users.noreply.github.com>
2022-12-20 11:03:15 +0000

    Complete day 20

---
+++ 20/python/part_two.py
@@ -0,0 +1,39 @@
+import sys
+
+DECRYPTION_KEY = 811589153
+numbers = [int(line.rstrip()) * DECRYPTION_KEY for line in sys.stdin]   
+
+n = len(numbers)
+
+pairs = [(i, num) for i, num in enumerate(numbers)]
+queue = pairs[:]
+
+for _ in range(10):
+    q = queue[:]
+    for _ in range(n):
+        item = q.pop(0)
+        i, num = item 
+
+        if num == 0: continue
+        idx = pairs.index(item)
+        new_idx = (idx + num) % (n - 1)
+
+        del pairs[idx]
+        if new_idx == 0:
+            pairs.append(item)
+        elif new_idx == n - 1:
+            pairs.insert(0, item)
+        else:
+            pairs.insert(new_idx, item)
+
+numbers = [num for _, num in pairs]
+
+grove = [1000, 2000, 3000]
+sum = 0
+for x in grove:
+    zero = numbers.index(0)
+    idx = (zero + x) % n
+
+    sum += numbers[idx]
+
+print(sum)
